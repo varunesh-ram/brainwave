01c76e3848cd6a8ec75a315603f9f5fd5c144122
varunesh-ram <varuneshram@yahoo.com>
2023-06-18 15:17:35 +0200

    initiaial commit

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React, {useEffect, useState} from 'react';
+import './App.css';
+import {ReactComponent as Bulb} from './bulb.svg';
+import cloud1 from './cloud1.png';
+import cloud2 from './cloud2.png';
+import cloud3 from './cloud3.png';
+import cloud4 from './cloud4.png';
+import cloud5 from './cloud5.png';
+import useMountTransition from "./useMountTransition";
+
+function App() {
+    const [borderStyle,setBorderStyle]= useState("button-dashed");
+    const [isLoadingPage,setIsLoadingPage]= useState(true);
+    const [isAgendaPage,setIsAgendaPage]= useState(false);
+
+    const [isMounted, setIsMounted] = useState(true);
+    const hasTransitionedIn = useMountTransition(isMounted, 200);
+
+    useEffect(()=>{
+        setTimeout(()=>{
+            setBorderStyle(borderStyle==="button-dashed"? "button-solid":"button-dashed");
+        },300)
+    },[borderStyle])
+
+    useEffect(() => {
+        if(isLoadingPage){
+            document.body.style.overflow = "hidden";
+        }
+        else{
+            document.body.style.overflow = "scroll"
+        }
+    },[isLoadingPage]);
+    return (
+        <div
+            style={{
+                background: 'linear-gradient(90deg, rgba(51,181,57,1) 0%, rgba(19,97,22,1) 65%)',
+                minHeight: '100vh',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}
+        >
+            {isLoadingPage && (hasTransitionedIn || isMounted)  &&
+            <div className={`title ${hasTransitionedIn && 'in'} ${isMounted && 'visible'}`}>
+                <div className={`bulb_container ${hasTransitionedIn && 'in'} ${isMounted && 'visible'}`}><Bulb/></div>
+                <div className={`title_container ${hasTransitionedIn && 'in'} ${isMounted && 'visible'}`}>
+                    <div className="title_text">
+                        brai <a className={borderStyle} style={{color:"gold",zIndex:"999"}} onClick={(e)=>{
+                            e.preventDefault();
+                            console.log("clicked");
+                            setIsLoadingPage(false);
+                            setIsAgendaPage(true);
+                        }}>NW</a> ave 3.0
+                    </div>
+                    <div className="left_subtext">
+                        Transforming IDEAs into REALity
+                    </div>
+                </div>
+            </div>}
+            {isAgendaPage &&
+                <div>
+                    <div className="he"></div>
+                </div>}
+
+            {/*<div className="clouds">
+                <img src={cloud1} alt="" className="clouds_one"></img>
+                <img src={cloud2} alt="" className="clouds_two"></img>
+                <img src={cloud3} alt="" className="clouds_three"></img>
+                <img src={cloud4} alt="" className="clouds_four"></img>
+                <img src={cloud5} alt="" className="clouds_five"></img>
+            </div>*/}
+        </div>
+    );
+}
+
+export default App;
---
+++ src/TitleAnimation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './TitleAnimation.css'; // Import the CSS file containing the styles
+
+const TitleAnimation = () => {
+    return (
+        <div className="arc-container">
+            <div className="arc"></div>
+            <div className="title">NW</div>
+        </div>
+    );
+};
+
+export default TitleAnimation;
